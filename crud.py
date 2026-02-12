557a267eccd5170cb630684f99bf7a3735d2ee4f
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-12 16:51:57 -0300

    feat: criando as funções de CRUD no banco.

---
+++ crud.py
@@ -0,0 +1,63 @@
+from .database import DBConnector
+from .auth import Auth
+from .models import Note, User
+
+class DatabaseManager:
+
+    def new_note(self, user_id : int, text : str):
+        with DBConnector.get_connection() as conn:
+            cursor = conn.cursor()
+            
+            cursor.execute(
+                """
+                INSERT INTO note (user_id, text)
+                VALUES (?, ?);
+                """, [user_id, text]
+            )
+
+            conn.commit()
+
+    def new_user(self, username : str, password : str):
+        password_hash = Auth.to_hash(password)
+
+        with DBConnector.get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO user (username, password_hash)
+                VALUES (?, ?);
+                """, [username, password_hash]
+            )
+        
+    def list_notes(self, user_id : int):
+
+        with DBConnector.get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                SELECT * FROM note WHERE user_id = ?;
+                """, [user_id]
+            )
+
+            results = cursor.fetchall()
+        if results:
+            return [Note.from_row(row) for row in results]
+        return results
+    
+    def find_user(self, username : str):
+        with DBConnector.get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                SELECT * FROM user WHERE username = ?;
+                """, [username]
+            )
+        
+            result = cursor.fetchone()
+        if result:
+            return User.from_row(result)
+        return result
+    
