d56131a825e7242cf0d47c7e312d0d0710570ba5
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-12 15:40:49 -0300

    feat: criando as funções de setup do banco

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+class DBConnector:
+    
+    @classmethod
+    def get_connection(cls):
+        return sqlite3.connect("notes.db")
+    
+
+class DBLauncher:
+
+    @classmethod
+    def create_tables(cls):
+        with DBConnector.get_connection() as conn:
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS user (
+                id INT PRIMARY KEY AUTOINCREMENT,
+                username VARCHAR(150) UNIQUE NOT NULL,
+                password_hash VARCHAR(100) NOT NULL
+                )
+                """
+            )
+
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS note (
+                id INT PRIMARY KEY AUTOINCREMENT,
+                user_id INT NOT NULL,
+                content TEXT,
+                FOREIGN KEY (user_id) REFERENCES user.id
+                ON DELETE CASCADE
+                )
+                """
+            )
+            
+                        
