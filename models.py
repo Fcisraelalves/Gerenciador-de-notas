df63a97a82524fc22770a2b6a347afc63dc883d1
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-02-12 19:48:27 -0300

    feat: criando os models para representar os dados do banco

---
+++ models.py
@@ -0,0 +1,26 @@
+from enum import StrEnum
+
+class Note:
+    FIELDS = ['id', 'user_id', 'text']
+    def __init__(self, id : int, user_id : int, text : str):
+        self.id = id
+        self.user_id = user_id
+        self.text = text
+
+    @classmethod
+    def from_row(cls, row):
+        kwargs = {field : value for field, value in zip(Note.FIELDS, row)}
+        return cls(**kwargs)
+    
+class User:
+    FIELDS = ['id', 'username', 'password_hash']
+    def __init__(self, id : int, username : str, password_hash : str):
+        self.id = id
+        self.username = username
+        self.password_hash = password_hash
+
+    @classmethod
+    def from_row(cls, self, row):
+        kwargs = {field : value for field, value in zip(User.FIELDS, row)}
+        return cls(**kwargs)
+    
